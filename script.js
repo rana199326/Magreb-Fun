3225ff31b3c617e2e4c5f0b9267cff8b574774bb
rana199326 <rana.hassan_93@yahoo.fr>
2023-11-02 16:46:28 +0100

    update page accueil AR

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const contactLink = document.querySelector('a[href="#contact"]');
-  
-    contactLink.addEventListener("click", function (event) {
-      event.preventDefault(); 
-  
-      const contactSection = document.getElementById("contact");
-  
-      contactSection.scrollIntoView({ behavior: "smooth" });
-    });
+  const contactLink = document.querySelector('a[href="#contact"]');
+
+  contactLink.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const contactSection = document.getElementById("contact");
+
+    contactSection.scrollIntoView({ behavior: "smooth" });
   });
-  
-  const images = [
-    "../images/image1.jpg",
-    "../images/image2.jpg",
-    "../images/image3.jpg",
+});
+
+const images = [
+  "../images/image1.jpg",
+  "../images/image2.jpg",
+  "../images/image3.jpg",
 ];
 
 let currentImageIndex = 0;
@@ -21,35 +21,58 @@ const banniereDyn = document.querySelector(".banniereDyn");
 const imgElement = banniereDyn.querySelector("img");
 
 function changeImage() {
-    imgElement.src = images[currentImageIndex];
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    imgElement.style.animation= "fade 10s infinite";
+  imgElement.src = images[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  imgElement.style.animation = "fade 10s infinite";
 }
 
 
 setInterval(changeImage, 10000);
 
-function maps(pays){
+function maps(pays) {
   switch (pays) {
-    case 'maroc':
-      document.getElementById ("mapmaroc").style.display='block' ;
-      break;
-    case 'mauritanie':
-      document.getElementById ("mapmauritanie").style.display = 'block'
-      break;
-    case 'algerie':
-      document.getElementById ("mapalgerie").style.display = 'block'
-      break;
-    case 'tunisie':
-      document.getElementById ("maptunisie").style.display = 'block'
-      break;
-    case 'libye':
-      document.getElementById ("maplibye").style.display = 'block'
-      break;
-  
-    default:
-      document.getElementById ("mapmaroc").style.display = 'none'
+    case 'المغرب':
+      document.getElementById("mapmaroc").style.display = 'block';
 
+      document.getElementById("map").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'block';
+      document.getElementById("nameCounty").innerText = pays;
+      break;
+    case 'موريتانيا':
+      document.getElementById("mapmauritanie").style.display = 'block'
+      document.getElementById("map").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'block';
+      document.getElementById("nameCounty").innerText = pays;
+      break;
+    case 'الجزائر':
+      document.getElementById("mapalgerie").style.display = 'block'
+      document.getElementById("map").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'block';
+      document.getElementById("nameCounty").innerText = pays;
+      break;
+    case 'تونس':
+      document.getElementById("maptunisie").style.display = 'block'
+      document.getElementById("map").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'block';
+      document.getElementById("nameCounty").innerText = pays;
+      break;
+    case 'ليبيا':
+      document.getElementById("maplibye").style.display = 'block'
+      document.getElementById("map").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'block';
+      document.getElementById("nameCounty").innerText = pays;
+      break;
+
+    default:
+      document.getElementById("mapmaroc").style.display = 'none';
+      document.getElementById("mapmauritanie").style.display = 'none';
+      document.getElementById("mapalgerie").style.display = 'none';
+      document.getElementById("maptunisie").style.display = 'none';
+      document.getElementById("maplibye").style.display = 'none';
+      document.getElementById("mapbtn").style.display = 'none';
+      document.getElementById("map").style.display = 'block';
+      document.getElementById("nameCounty").innerText = '';
       break;
   }
+
 }
